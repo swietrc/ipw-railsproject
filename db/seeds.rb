69aece7ff87ec4350d33e6d645a32118d4d9e53c
swietrich <swietrich@etu.unistra.fr>
2017-03-30 10:19:39 +0200

    Add article & change erb to haml

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+articles = Article.create([
+    { title: 'Le Jambon c\'est très bon', body: "TMTC" }, 
+    { title: "On a découvert le vrai nom de Fillon", body: "FION PUTAIN!" }, 
+    { title: "Donald Trump a un cancer du cul !", body: "Si si je vous assure !"}, 
+    { title: "Donald Trump a un cancer du cul !", body: "Si si je vous assure !"},
+    { title: "Donald Trump a un cancer du cul !", body: "Si si je vous assure !"},
+    { title: "Donald Trump a un cancer du cul !", body: "Si si je vous assure !"},
+    { title: "Donald Trump a un cancer du cul !", body: "Si si je vous assure !"},
+    { title: "Donald Trump a un cancer du cul !", body: "Si si je vous assure !"},
+    { title: "Donald Trump a un cancer du cul !", body: "Si si je vous assure !"},
+    { title: "Donald Trump a un cancer du cul !", body: "Si si je vous assure !"},
+    { title: "Donald Trump a un cancer du cul !", body: "Si si je vous assure !"}
+])
